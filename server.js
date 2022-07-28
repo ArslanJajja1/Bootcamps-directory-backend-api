80dd2a5ffb8670226908780ed993718ab3ee43fd
ArslanJajja1 <arslanjajja13@gmail.com>
2022-07-28 09:32:05 +0500

    morgan -logging middleware

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 // Import bootcamp files
 const bootcamps = require("./routes/bootcamps");
+const morgan = require("morgan");
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
 const app = express();
-
+// Development logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 app.get("/", (req, res) => {
     res.send("Hi from express js");
 });
