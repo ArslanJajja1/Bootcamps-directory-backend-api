f4b86211a827ce4fe50a648fdcdab360ffa0e41a
ArslanJajja1 <arslanjajja13@gmail.com>
2022-07-29 00:04:19 +0500

    small change in Errorhandler middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,9 @@
 const errorHandler = (err, req, res, next) => {
     // log to console for developer
     console.log(err);
-    res.status(500).json({ success: false, error: err.message });
+    res.status(err.statusCode || 500).json({
+        success: false,
+        error: err.message || "Server Error",
+    });
 };
 module.exports = errorHandler;
