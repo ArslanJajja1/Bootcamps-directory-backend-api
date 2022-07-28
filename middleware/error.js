1bef17ed3235316420274ba4c7c2a0ef5e106530
ArslanJajja1 <arslanjajja13@gmail.com>
2022-07-29 00:44:37 +0500

    handling mongoose duplicate key error with errorHandler middleware

--- middleware/error.js
+++ middleware/error.js
@@ -5,10 +5,15 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
     // log to console for developer
     console.log(err);
-    // Mongoose bad objectId
+    // Mongoose bad objectId error
     if (err.name === "CastError") {
         const message = `Resource not found with id of ${err.value}`;
         error = new ErrorResponse(message, 404);
+    }
+    // Mongoose duplicate key error
+    if (err.code === 11000) {
+        const message = "Duplicate field value entered";
+        error = new ErrorResponse(message, 400);
     }
     res.status(error.statusCode || 500).json({
         success: false,
