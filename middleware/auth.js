feedcc611b527a9ea3104ed9fbb88182e9e85867
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-02 16:23:50 +0500

    auth protect middleware to protect routes

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("./async");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
+
+//* Protect Routes
+exports.protect = asyncHandler(async (req, res, next) => {
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+    //* Make sure token exits
+    if (!token) {
+        return next(new ErrorResponse(`Not authorized this route`, 401));
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded);
+        req.user = await User.findById(decoded.id);
+        next();
+    } catch (error) {
+        return next(new ErrorResponse(`Not authorized this route`, 401));
+    }
+});
