fce1a0d936105d6296980cdabe5b4ddaacfba4c8
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-02 16:32:21 +0500

    decoded id fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -19,7 +19,8 @@ exports.protect = asyncHandler(async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         console.log(decoded);
-        req.user = await User.findById(decoded.id);
+        req.user = await User.findById(decoded._id);
+        console.log("user", req.user);
         next();
     } catch (error) {
         return next(new ErrorResponse(`Not authorized this route`, 401));
