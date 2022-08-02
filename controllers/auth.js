be3bba6e61b7568ea9bf15fbbb86a2a6fce13727
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-02 14:54:38 +0500

    register user,send token in response

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,5 +8,7 @@ const errorHandler = require("../middleware/error");
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body;
     const user = await User.create({ name, email, password, role });
-    res.status(200).json({ success: true });
+    // Create token
+    const token = user.getSignedJwtToken();
+    res.status(200).json({ success: true, token });
 });
