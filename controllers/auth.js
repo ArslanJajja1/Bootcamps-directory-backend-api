c878c92d2b5b006a5b8553e7dbd416157d701397
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-02 14:37:05 +0500

    user registration controller method

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,7 @@ const errorHandler = require("../middleware/error");
 //*  @ Route       POST /api/v1/auth/register
 //*  @ Access      Public
 exports.register = asyncHandler(async (req, res, next) => {
+    const { name, email, password, role } = req.body;
+    const user = await User.create({ name, email, password, role });
     res.status(200).json({ success: true });
 });
