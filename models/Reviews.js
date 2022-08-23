dc28fb13bc41297042107b049996bfdb64ac5b77
ArslanJajja1 <arslanjajja13@gmail.com>
2022-08-23 15:55:29 +0500

    aggregate - calculate average rating for bootcamp

--- models/Reviews.js
+++ models/Reviews.js
@@ -34,4 +34,35 @@ const ReviewSchema = new mongoose.Schema({
 });
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+// static method to get average of course tuitions
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId },
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                averageRating: { $avg: "$rating" },
+            },
+        },
+    ]);
+    try {
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            averageRating: obj[0].averageRating,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Call average cost after save
+ReviewSchema.post("save", async function () {
+    await this.constructor.getAverageRating(this.bootcamp);
+});
+// Call average cost before remove
+ReviewSchema.pre("remove", async function () {
+    await this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
